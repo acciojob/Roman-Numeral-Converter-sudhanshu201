d2103b0433a425e4c352370b30ed9d6abe928d5f
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2023-01-22 20:13:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function intToRoman(num) {
+function convertToRoman(num) {
+  	const obj = {
+      0:['M',1000], 
+      1:['D', 500], 
+      2:['C', 100], 
+      3:['L', 50], 
+      4:['X', 10], 
+      5:['V', 5], 
+      6:['I', 1]
+    };
+
+  //your code here
+	
     let romanNumeral = "";
     const romanNumerals = {
         M: 1000,
@@ -24,8 +36,9 @@ function intToRoman(num) {
     }
 
     return romanNumeral;
-}
 
+
+}
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
